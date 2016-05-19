724edf02a210b3b63b7a0f500e551c73d43046be
nwre87 <nwre87@gmail.com>
2016-05-18 23:34:13 -1000

    modifies hero-detail component when top heroes clicked from dashboard

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router-deprecated';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -9,12 +11,18 @@ import { HeroService } from './hero.service';
 
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit() {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1,5))
   }
 
-  goToDetail(){}
+  goToDetail(hero: Hero){
+    let link = ['HeroDetail', { id: hero.id }]
+    this.router.navigate(link);
+  }
 }
